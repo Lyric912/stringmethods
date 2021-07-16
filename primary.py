4ac3becc025db21c44f67bab108872e3bf2e5f82
Lyric Marner <86728890+Lyric912@users.noreply.github.com>
2021-07-16 20:09:39 +0000

    the primary and question file

--- primary.py
+++ primary.py
@@ -1,5 +1,5 @@
-# author:
-# date:
+# author: Lyric Marner
+# date: July 13, 2021
 
 # --------------- Section 1 --------------- #
 
@@ -10,23 +10,43 @@
 #   b. Print the variable in all upper case.
 #   c. Print the variable in all lower case.
 #   d. Print the variable capitalized (Look to documentation.)
-
-
+name = 'Lyric Marner'
+print(name. center(30, '-'))
+print(name.upper())
+print(name.lower())
+print(name.capitalize())
+print()
 # 2 | String Methods
 #
 # 1 - Prompt input from the user, asking them to enter a sentence. Save it to a variable named text.
 #   a. Find the first instance of the letter a. Print that position.
 #   b. Find the first instance of the letter b. Print that position.
 #   c. Find the first instance of a word of your choice. Print that position.
-
-
+text = input('Please enter a sentence: ')
+print('Position of the letter a: ', text.find('a'))
+print('Position of the letter b: ', text.find('b'))
+print('Position of the word lovely: ', text.find('lovely'))
+print()
 # 3 | String Methods
 #
 # 1 - Prompt input from the user, asking them to enter a sentence. Save it to a variable named text.
-#   a. Find the position of every vowel in text. Save them each to a variable.
+#   a. Find the first appearence of every vowel in the text.
 #   b. Using a built-in function, print the position of the vowel that shows up last.
 #   c. Using a built-in function, print the position of the vowel that shows up first.
+text = input('Please enter a sentence: ')
+text = text.lower()
+a_position = text.find('a')
+e_position = text.find('e')
+i_position = text.find('i')
+o_position = text.find('o')
+u_position = text.find('u')
 
+print('Position of the vowel a: ', a_position)
+print('Position of the vowel e: ', e_position)
+print('Position of the vowel i: ', i_position)
+print('Position of the vowel o: ', o_position)
+print('Position of the vowel u: ', u_position)
+print()
 
 # 4 | String Indexing
 #
@@ -37,6 +57,15 @@
 #       HINT: There are multiple ways of doing this. Is there a function that we can use that will find
 #           the position of the last letter, or atleast one off from it?
 
+text = input('Enter a sentence: ')
+print('The 0th letter in the text is', text[0])
+print('The first letter of the text is:', text[1])
+print('The second letter of the text is:', text[2])
+print('The third letter of the text is:', text[3])
+#print(text[1], text[2], text[3])
+length = len(text)
+print('The last letter of the text is:', text[length - 1])
+print()
 
 # 5 | String Slicing
 #
@@ -46,7 +75,13 @@
 #   c. Slice text from 3rd position to end. Print that.
 #   d. Slice text from the beginning to 5 positions before the last character. Print that.
 #       HINT: Use a function to get the last position of the string.
-
+text = input('Please enter a sentence: ')
+print(text[2:5])
+print(text[0:8])
+print(text[3:])
+last_char = len(text)
+print(text[0:last_char - 5])
+print()
 
 # 6 | String Slicing
 #
@@ -54,4 +89,7 @@
 #   a. Print the text, but only every 2nd character.
 #   b. Print the text, but only every 3rd character.
 #   c. Print the text, but in reverse order.
-
+text = input('Enter a sentence: ')
+print(text[::2])
+print(text[::3])
+print(text[::-1])
